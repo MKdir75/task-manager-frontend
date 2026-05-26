f19399e6e106780258e66d60b5240efe30085e1b
Prince Mahamud <pallensteve105@gmail.com>
2026-05-26 23:39:11 +0600

    add .env file

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:4000/api" });
+const API = axios.create({ 
+  baseURL: process.env.REACT_APP_API_URL,
+});
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("token")) {
